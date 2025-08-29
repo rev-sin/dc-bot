feedbb0a5b250612a501c258d7509e479670ed4f
Revanth Singothu <revanthsingothu@gmail.com>
2025-08-29 00:52:20 +0000

    add: pre-commit hook

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged on as {self.user}!")
+
+    async def on_message(self, message):
+        print(f"Message from {message.author}: {message.content}")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv("BOT_TOKEN"))
